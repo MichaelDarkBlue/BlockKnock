ae33bcdf7cfc6c66cb86007180635f2a17b06953
MichaelDarkBlue <michaeldarkblue@gmail.com>
2015-02-26 20:36:00 -0500

    Revert "Force Text Assets"
    This reverts commit cd5052a4dfafd62ba7b7556dbaf5d55592ac4cb2.

--- Assets/scripts/wall.cs
+++ Assets/scripts/wall.cs
@@ -5,7 +5,6 @@ public class wall : MonoBehaviour {
 
 	public Transform prefabCube1;
 	private float[] numList = {.2f,.4f,.6f,.8f};
-    public GUIText gui; 
 	// Use this for initialization
 	void Start () {
 		//int counter = 1;
@@ -17,13 +16,13 @@ public class wall : MonoBehaviour {
 			for (int x = 0; x < am; x++) {
 				for (int z = 0; z < am; z++) {
 					//GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-					//cube.AddComponent<Rigidbody>();   
-					//cube.transform.position = new Vector3(x+xo, y+yo, z+zo);   
+					//cube.AddComponent<Rigidbody>();
+					//cube.transform.position = new Vector3(x+xo, y+yo, z+zo);  
 					//cube.tag = "cube1";
-                    GameObject c = (GameObject)Instantiate(prefabCube1, new Vector3(x + xo, y + yo, z + zo), Quaternion.identity);
+					Instantiate(prefabCube1, new Vector3(x+xo, y+yo, z+zo),Quaternion.identity);
 					//prefabCube1.tag = "cube" + counter.ToString(); 
 					//counter++;
-					c.renderer.material.color = randomColor();
+					prefabCube1.renderer.material.color = randomColor();
 					//Debug.Log("x: " + (x+xo)); 
 				}
 			}
@@ -40,12 +39,11 @@ public class wall : MonoBehaviour {
 
 		//int x = Random.Range (0, 3);
 		int min = 0; int max = 3 + 1;
-        int x = Random.Range(min, max), y = Random.Range(min, max), z = Random.Range(min, max);
-        gui.text += x.ToString() + y.ToString() + z.ToString() + ",";
+		GUIText gui; gui.text += Random.Range (min, max).ToString;
 		return new Color(
-			numList[x],
-			numList[y],
-			numList[z]
+			numList[Random.Range (min, max)],
+			numList[Random.Range (min, max)],
+			numList[Random.Range (min, max)]
 		                 );
 
 	}
