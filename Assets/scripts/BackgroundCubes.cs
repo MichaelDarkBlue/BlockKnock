eb3a6ed5205c6247cd2000e93788db1a3ff9919e
JosephMA007 <josephma007@gmail.com>
2015-02-28 08:29:34 -0500

    Added Background Cube Script

---
+++ Assets/scripts/BackgroundCubes.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundCubes : MonoBehaviour
+{
+	void Start()
+	{
+		//Random Color 
+		//renderer.material.color = new Color(0, 255, 0);
+		Vector3 color = new Vector3(Random.Range(0.0F, 0.5F), Random.Range(0.0F, 0.5F), Random.Range(0.0F, 0.5F));
+		renderer.material.color = new Color(color.x, color.y, color.z);
+		//Random Size
+		transform.localScale += new Vector3(Random.Range(1.0F, 3.0F), Random.Range(1.0F, 3.0F), Random.Range(1.0F, 3.0F));
+		//Random Rotation
+		rigidbody.angularVelocity = Random.insideUnitSphere;
+	}
+}
