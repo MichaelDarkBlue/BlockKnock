950c06d16b36b4ebd60f0d978d787448009f6e0d
JosephMA007 <josephma007@gmail.com>
2015-03-01 20:23:18 -0500

    Added Networking to Wall Script

--- Assets/scripts/wall.cs
+++ Assets/scripts/wall.cs
@@ -5,36 +5,35 @@ public class wall : MonoBehaviour {
 
 	public Transform prefabCube1;
 	private float[] numList = {.2F,.4F,.6F,.8F};
+	bool spawnedCubes;
 	// Use this for initialization
-	void Start () {
-		//int counter = 1;
-		int am = 5;
-		int xo = -2;
-		int yo = 1;
-		int zo = -2;
-		for (int y = 0; y < am; y++) {
-			for (int x = 0; x < am; x++) {
-				for (int z = 0; z < am; z++) {
-					//GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-					//cube.AddComponent<Rigidbody>(); 
-					//cube.transform.position = new Vector3(x+xo, y+yo, z+zo);    
-					//cube.tag = "cube1";
-                    GameObject c;
-                    c = Instantiate(prefabCube1, new Vector3(x+xo, y+yo, z+zo),Quaternion.identity) as GameObject;
-					//prefabCube1.tag = "cube" + counter.ToString(); 
-					//counter++;
-                    c.tag = "cube1";
-					c.renderer.material.color = randomColor();
-					//Debug.Log("x: " + (x+xo)); 
-				}
-			}
-		}
-
-
-	}
+//	void Start () {
+//
+//	}
 	// Update is called once per frame 
 	void Update () {
-
+		if (spawnedCubes == false & Network.isServer) {
+			//int counter = 1;
+			int am = 5;
+			int xo = -2;
+			int yo = 1;
+			int zo = -2;
+			for (int y = 0; y < am; y++) {
+				for (int x = 0; x < am; x++) {
+					for (int z = 0; z < am; z++) {
+						//GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+						//cube.AddComponent<Rigidbody>(); 
+						//cube.transform.position = new Vector3(x+xo, y+yo, z+zo);    
+						//cube.tag = "cube1";
+						Network.Instantiate(prefabCube1, new Vector3 (x + xo, y + yo, z + zo), Quaternion.identity, 0);
+						//prefabCube1.tag = "cube" + counter.ToString(); 
+						//counter++;
+						//Debug.Log("x: " + (x+xo)); 
+					}
+				}
+			}
+			spawnedCubes = true;
+		}
 	}
 
 	private Color randomColor () {
